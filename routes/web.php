fbdadf62acec3b1d90ce56426d2107f1860023e5
dima.moisieienko <dima.moisieienko>
2025-07-11 16:24:20 +0300

    created model Job

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
+
     return view('jobs', [
      'jobs' => Job::all()
     ]);
@@ -19,10 +20,7 @@ Route::get('/contact', function () {
 });
 
 Route::get('/jobs/{id}', function ($id) {
-  $job = Job::find($id);
+    $job = Job::find($id);
 
-  return view('job', ['job' => $job]);
+    return view('job', ['job' => $job]);
 });
-
-
-

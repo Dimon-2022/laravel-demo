e938cc660b7d90e138500f8141b51e4f8be97392
dima.moisieienko <dima.moisieienko>
2025-07-20 17:14:44 +0300

    implemented CRUD for resource Job

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -5,6 +5,11 @@
    
    <h2 class="font-bold text-lg">{{$job['title']}}</h2>
    <p>This job pays {{$job['salary']}} per year.</p>
-
+   {{-- <a href="/jobs/create">Edit job</a> --}}
+   <p class="mt-6">
+      <x-button href="/jobs/{{$job->id}}/edit">
+         Edit job
+      </x-button>
+   </p>
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@ Route::get('/', function () {
     return view('home');
 });
 
+
+//Index  -  show all jobs
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
     return view('jobs.index', [
@@ -18,13 +20,16 @@ Route::get('/jobs', function () {
     ]);
 });
 
+
+
+//store - persist data in db
 Route::post('/jobs', function () {
     request()->validate([
         'title' => ['required', 'min:3'],
         'salary' => ['required']
     ]);
-    
-    
+
+
     Job::create([
         'title' => request('title'),
         'salary' => request('salary'),
@@ -80,12 +85,59 @@ Route::get('/contact', function () {
     return view('contact');
 });
 
+
+//create - show form for creating new job
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+
+//show - show single job
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
+});
+
+//edit - show form for editing particular job
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+//update - update job
+Route::patch('/jobs/{id}', function ($id) {
+    //validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+
+    //     //update job
+    $job = Job::findOrFail($id);
+    // $job->title = request('title');
+    // $job->salary = request('salary');
+    // $job->save();
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+
+    //redirect
+    return redirect('/jobs/' . $job->id);
+});
+
+
+//destroy - delete job
+Route::delete('/jobs/{id}', function ($id) {
+    //delete
+    $job = Job::findOrFail($id);
+    $job->delete();
+
+    //redirect to /jobs
+    return redirect('/jobs');
 });
